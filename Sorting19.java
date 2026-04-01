d0c89741e275c0502244fbee45c58b822941a10b
“radennobioyetgit config --global user.name “radennobioyet <radensnpmb@gmail.com>
2026-04-01 10:34:03 +0700

    selection sort

--- Sorting19.java
+++ Sorting19.java
@@ -1,16 +1,16 @@
 public class Sorting19 {
 
     int[] data;
-    int jmlData;
+    int jumData;
 
-    Sorting19(int[] data, int jmlData) {
+    Sorting19(int[] data, int jumData) {
         this.data = data;
-        this.jmlData = jmlData;
+        this.jumData = jumData;
     }
 
     void bubbleSort() {
-        for (int i = 0; i < jmlData - 1; i++) {
-            for (int j = 0; j < jmlData - i - 1; j++) {
+        for (int i = 0; i < jumData - 1; i++) {
+            for (int j = 0; j < jumData - i - 1; j++) {
                 if (data[j] > data[j + 1]) {
                     int tmp = data[j];
                     data[j] = data[j + 1];
@@ -20,8 +20,22 @@ public class Sorting19 {
         }
     }
 
+    void SelectionSort() {
+        for (int i = 0; i < jumData - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < jumData; j++) {
+                if (data[j] < data[min]) {
+                    min = j;
+                }
+            }
+            int temp = data[i];
+            data[i] = data[min];
+            data[min] = temp;
+        }
+    }
+
     void tampil() {
-        for (int i = 0; i < jmlData; i++) {
+        for (int i = 0; i < jumData; i++) {
             System.out.print(data[i] + " ");
         }
         System.out.println();
--- SortingMain19.java
+++ SortingMain19.java
@@ -1,14 +1,22 @@
 public class SortingMain19 {
     public static void main(String[] args) {
         int a[] = { 20, 10, 2, 7, 12 };
-       
-        Sorting19 dataurut1 = new Sorting19(a, a.length);
 
+        Sorting19 dataurut1 = new Sorting19(a, a.length);
         System.out.println("Data Awal 1");
         dataurut1.tampil();
         dataurut1.bubbleSort();
         System.out.println("Data Sudah di Urutkan dengan BUBBLE SORT (ASC)");
         dataurut1.tampil();
+
+        int b[] = { 30, 20, 2, 8, 14 };
+
+        Sorting19 dataurut2 = new Sorting19(b, b.length);
+        System.out.println("Data awal 2");
+        dataurut2.tampil();
+        dataurut2.SelectionSort();
+        System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
+        dataurut2.tampil();
     }
 
 }
