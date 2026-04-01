6d37444f7fcbc8ab712c296407a5f2492fe55614
“radennobioyetgit config --global user.name “radennobioyet <radensnpmb@gmail.com>
2026-04-01 11:23:07 +0700

    percobaan 2 selection sort

--- MahasiswaDemo19.java
+++ MahasiswaDemo19.java
@@ -1,23 +1,27 @@
 public class MahasiswaDemo19 {
     public static void main(String[] args) {
-    MahasiswaBerprestasi9 list = new MahasiswaBerprestasi9();
-    Mahasiswa19 m1 = new Mahasiswa19("123", "Zidan", "2A", 3.2);
-    Mahasiswa19 m2 = new Mahasiswa19("124", "Ayu", "2A", 3.5);
-    Mahasiswa19 m3 = new Mahasiswa19("125", "Sofi", "2A", 3.1);
-    Mahasiswa19 m4 = new Mahasiswa19("126", "Sita", "2A", 3.9);
-    Mahasiswa19 m5 = new Mahasiswa19("127", "Miki", "2A", 3.7);
+        MahasiswaBerprestasi9 list = new MahasiswaBerprestasi9();
+        Mahasiswa19 m1 = new Mahasiswa19("123", "Zidan", "2A", 3.2);
+        Mahasiswa19 m2 = new Mahasiswa19("124", "Ayu", "2A", 3.5);
+        Mahasiswa19 m3 = new Mahasiswa19("125", "Sofi", "2A", 3.1);
+        Mahasiswa19 m4 = new Mahasiswa19("126", "Sita", "2A", 3.9);
+        Mahasiswa19 m5 = new Mahasiswa19("127", "Miki", "2A", 3.7);
 
-    list.tambah(m1);
-    list.tambah(m2);
-    list.tambah(m3);
-    list.tambah(m4);
-    list.tambah(m5);
+        list.tambah(m1);
+        list.tambah(m2);
+        list.tambah(m3);
+        list.tambah(m4);
+        list.tambah(m5);
 
-    System.out.println("Data mahasiswa sebelum sorting: ");
-    list.tampil();
+        System.out.println("Data mahasiswa sebelum sorting: ");
+        list.tampil();
 
-    System.out.println("Data Mahasiswa setelah sorting berdasarkan IPK (DESC) : ");
-    list.bubbleSort();
-    list.tampil();
-}
+        System.out.println("Data Mahasiswa setelah sorting berdasarkan IPK (DESC) : ");
+        list.bubbleSort();
+        list.tampil();
+
+        System.out.println("Data yang sudah terurut menggunakan SELECTION SORT (ASC)");
+        list.selectionSort();
+        list.tampil();
+    }
 }
