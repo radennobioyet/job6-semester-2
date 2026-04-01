7db5cc8b4f4dd064b8c06a2eb069d9d59366e82c
“radennobioyetgit config --global user.name “radennobioyet <radensnpmb@gmail.com>
2026-04-01 10:37:32 +0700

    insertion sort

--- SortingMain19.java
+++ SortingMain19.java
@@ -17,6 +17,15 @@ public class SortingMain19 {
         dataurut2.SelectionSort();
         System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
         dataurut2.tampil();
+
+        int c[] = {40, 10, 4, 9, 3};
+
+        Sorting19 dataurut3 = new Sorting19(c, c.length);
+        System.out.println("Data awal 3");
+        dataurut3.tampil();
+        dataurut3.SelectionSort();
+        System.out.println("Data sudah diurutkan dengan INSERTION SORT (ASC)");
+        dataurut3.tampil();
     }
 
 }
