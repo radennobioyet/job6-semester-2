b90e510d6caf5bbd794aedf65f738cd1f8a0f79a
“radennobioyetgit config --global user.name “radennobioyet <radensnpmb@gmail.com>
2026-04-01 11:14:30 +0700

    percobaan 2

---
+++ MahasiswaBerprestasi9.java
@@ -0,0 +1,32 @@
+public class MahasiswaBerprestasi9 {
+    Mahasiswa19[] listMhs = new Mahasiswa19[5];
+    int idx;
+
+    void tambah(Mahasiswa19 m) {
+        if (idx < listMhs.length) {
+            listMhs[idx] = m;
+            idx++;
+        } else {
+            System.out.println("data sudah penuh");
+        }
+    }
+
+    void tampil() {
+        for (Mahasiswa19 m : listMhs) {
+            m.tampilInformasi();
+            System.out.println("-------------------------");
+        }
+    }
+
+    void bubbleSort() {
+        for (int i = 0; i < listMhs.length - 1; i++) {
+            for (int j = 1; j < listMhs.length - i; j++) {
+                if (listMhs[j].ipk > listMhs[j - 1].ipk) {
+                    Mahasiswa19 tmp = listMhs[j];
+                    listMhs[j] = listMhs[j - 1];
+                    listMhs[j - 1] = tmp;
+                }
+            }
+        }
+    }
+}
